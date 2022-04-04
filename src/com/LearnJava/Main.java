5591d2b83348d05f66baac7b289027d4273388a0
Sebastian <sebzo65@gmail.com>
2022-04-05 08:52:45 +1000

    Improved the readability of existing code by further modularising it and indicating which method performs which function. Now there is a menu that lists a few properties of each number (even, odd, buzz and duck - i.e. whether the number contains trailing zeros), as well as validating that it is a natural number. Scanner input is imported into a centralised method that handles output, allowing the main method to remain quite clean

--- src/com/LearnJava/Main.java
+++ src/com/LearnJava/Main.java
@@ -5,13 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Enter a natural number:");
         Scanner scanner = new Scanner(System.in);
-        int userInput = scanner.nextInt();
 
         NumberMethods numberMethods = new NumberMethods();
-        numberMethods.checkNaturalNum(userInput);
-        numberMethods.checkEvenOdd(userInput);
-        numberMethods.checkBuzzNum(userInput);
+        numberMethods.executeMenu(scanner);
     }
 }
